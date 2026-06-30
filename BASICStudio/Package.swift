891d09e97997391876b9181a009ffdb005d6ca92
Bobby Skinner <bobbyski@mac.com>
2026-06-30 00:20:04 -0400

    VTG complete

--- BASICStudio/Package.swift
+++ BASICStudio/Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     dependencies: [
         .package(path: "../BASICCore"),
         .package(url: "https://github.com/gonzalezreal/swift-markdown-ui", from: "2.4.1"),
-        .package(url: "https://github.com/bobbyski/SwiftTerm.git", from: "1.5.5"),
-        .package(url: "https://github.com/bobbyski/VectorTerminalSDK.git", from: "1.5.5")
+        .package(url: "https://github.com/bobbyski/SwiftTerm.git", from: "1.5.6"),
+        .package(url: "https://github.com/bobbyski/VectorTerminalSDK.git", from: "1.5.6")
     ],
     targets: [
         .executableTarget(
